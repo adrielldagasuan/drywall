b94189ba46411706181e4dd781cf2affdf7ab867
Adriell Matthew Julius Dagasuan <adrielldagasuan@gmail.com>
2016-08-16 12:48:29 +0800

    fixed push to github

--- deploy/deploy-aws.sh
+++ deploy/deploy-aws.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker push adrielldagasuan/products-service:$CIRCLE_SHA1
+docker push adrielldagasuan/user-management:$CIRCLE_SHA1
 ansible-playbook deploy/ecs-deploy.yml
--- deploy/deploy-gke.sh
+++ deploy/deploy-gke.sh
@@ -4,7 +4,7 @@
 set -e
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker push adrielldagasuan/products-service:$CIRCLE_SHA1
+docker push adrielldagasuan/user-management:$CIRCLE_SHA1
 sudo /opt/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment products-service -p '{"spec":{"template":{"spec":{"containers":[{"name":"products-service","image":"docker.io/adrielldagasuan/products-service:'"$CIRCLE_SHA1"'"}]}}}}'
