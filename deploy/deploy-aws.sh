301ea8edb761cf5ff6f56b35a95036f1c6786f27
Adriell Matthew Julius Dagasuan <adrielldagasuan@gmail.com>
2016-08-16 11:47:01 +0800

    fixed deployment for updates

--- deploy/deploy-aws.sh
+++ deploy/deploy-aws.sh
@@ -2,4 +2,4 @@
 
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 docker push adrielldagasuan/products-service:$CIRCLE_SHA1
-ansible-playbook deploy/ec2-cloudformation.yml
+ansible-playbook deploy/ecs-deploy.yml
